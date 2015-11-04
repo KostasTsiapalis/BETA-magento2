d6e4506996b36e1c329d525c74e0c59101c952c0
Jim Simon <jim@blueacorn.com>
2015-11-04 13:05:03 -0500

    Setting up module

---
+++ app/code/Traning3/ExchangeRate/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     ${MAGENTO_MODULE_NAMESPACE}\\${MAGENTO_MODULE}
+ * @version
+ * @author      Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Training3_ExchangeRate',
+    __DIR__
+);
