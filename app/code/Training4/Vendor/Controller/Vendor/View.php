ef359f91ff4dd644057c5d168348931c4d9a3c06
Sam Tay <sam.chong.tay@gmail.com>
2015-12-06 21:58:47 -0500

    Add page for vendor list view

---
+++ app/code/Training4/Vendor/Controller/Vendor/View.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package     Training4\Vendor
+ * @version     0.1.0
+ * @author      Sam Tay @ Blue Acorn, Inc. <code@blueacorn.com>
+ * @copyright   Copyright © 2015 Blue Acorn, Inc.
+ */
+namespace Training4\Vendor\Controller\Vendor;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class View extends Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        $this->_resultPageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        return $resultPage;
+    }
+}
